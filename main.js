e053351cf1f7ff434d71b3d16317470c5387d7bd
KushRohra <iec2017024@iiita.ac.in>
2020-02-12 01:59:53 +0530

    done

---
+++ main.js
@@ -0,0 +1,21 @@
+$('#animateBtn').click(function(){
+    animate('#anim-head', 'bounce');
+    animate('#anim-body', 'rubberBand');
+    return false;
+});
+
+$('#animateBtn2').click(function(){
+    animate('header', 'slideOutUp');
+    setTimeout(function(){
+        $('header').css('visibility','hidden');
+    }, 1000);
+    return false;
+});
+
+function animate(element, animation)
+{
+    $(element).addClass('animated '+animation);
+    var wait = setTimeout(function(){
+      $(element).removeClass('animated '+animation);
+    }, 1000);
+}
